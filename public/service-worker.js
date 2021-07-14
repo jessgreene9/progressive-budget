f8929822af5e5dc7675dc373f861337962514591
Jess Greene <jess.greene9@gmail.com>
2021-07-14 09:38:21 -0400

    added indexedDB, fixed files to be cached in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,14 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 
 const FILES_TO_CACHE = [
-   
+   '/',
+   '/index.html',
+   '/manifest.webmanifest',
+   '/db.js',
+   '/index.js',
+   '/styles.css',
+   '/icons/icon-192x192.png',
+   '/icons/icon-512x512.png'
   ];
   
   
